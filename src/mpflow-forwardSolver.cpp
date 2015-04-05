9190f5649a8b621b77b970ec8d00dd437adb8378
schansge <patrik@schnuffmade.com>
2015-04-05 20:23:35 +0200

    added mesh info to output

--- src/mpflow-forwardSolver.cpp
+++ src/mpflow-forwardSolver.cpp
@@ -66,20 +66,15 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    str::print("----------------------------------------------------");
-    str::print("Initialize mpFlow forward solver");
-    time.restart();
-
     // Create model helper classes
     auto electrodes = createBoundaryDescriptorFromConfig(modelConfig["electrodes"], modelConfig["mesh"]["radius"].u.dbl);
     auto source = createSourceFromConfig<dataType>(modelConfig["source"], electrodes, cudaStream);
 
+    time.restart();
+    str::print("----------------------------------------------------");
+
     // load mesh from config
     auto mesh = createMeshFromConfig(modelConfig["mesh"], path, electrodes);
-
-    // Create main model class
-    auto equation = std::make_shared<FEM::Equation<dataType, FEM::basis::Linear, false>>(
-        mesh, source->electrodes, modelConfig["referenceValue"].u.dbl, cudaStream);
 
     // load predefined gamma distribution from file, if path is given
     std::shared_ptr<numeric::Matrix<dataType>> gamma = nullptr;
@@ -87,8 +82,19 @@ int main(int argc, char* argv[]) {
         gamma = numeric::Matrix<dataType>::loadtxt(str::format("%s/%s")(path, std::string(modelConfig["gammaFile"])), cudaStream);
     }
     else {
-        gamma = std::make_shared<numeric::Matrix<dataType>>(mesh->elements.rows(), 1, cudaStream, 1.0f);
+        gamma = std::make_shared<numeric::Matrix<dataType>>(mesh->elements.rows(), 1, cudaStream, dataType(1));
     }
+
+    str::print("Mesh loaded with", mesh->nodes.rows(), "nodes and", mesh->elements.rows(), "elements");
+    str::print("Time:", time.elapsed() * 1e3, "ms");
+
+    str::print("----------------------------------------------------");
+    str::print("Initialize mpFlow forward solver");
+    time.restart();
+
+    // Create main model class
+    auto equation = std::make_shared<FEM::Equation<dataType, FEM::basis::Linear, false>>(
+        mesh, source->electrodes, modelConfig["referenceValue"].u.dbl, cudaStream);
 
     // Create forward solver and solve potential
     // use different numeric solver for different source types
